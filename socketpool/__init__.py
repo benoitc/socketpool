bcbce5b5ceeb86f1520fbd809ff150eafbf64c5f
benoitc <bchesneau@gmail.com>
2012-01-24 14:15:50 -0800

    export useful objects

---
+++ socketpool/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -
+#
+# This file is part of socketpool released under the MIT license.
+# See the NOTICE for more information.
+
+from socketpool.pool import ConnectionPool
+from socketpool.conn import Connector, TcpConnector
