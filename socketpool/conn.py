0862dd3c9cb031e097e67598cb4fced193d855d0
Mocker <lsin30@foxmail.com>
2015-05-05 14:05:03 +0800

    add unix socket connector
    add unix socket connector

--- socketpool/conn.py
+++ socketpool/conn.py
@@ -3,7 +3,6 @@
 # This file is part of socketpool.
 # See the NOTICE for more information.
 
-import select
 import socket
 import time
 import random
@@ -25,6 +24,56 @@ class Connector(object):
 
     def invalidate(self):
         raise NotImplementedError()
+
+
+class UnixConnector(Connector):
+
+    def __init__(self, socket_file, backend_mod, pool=None):
+        self._s = backend_mod.Socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.socket_file = socket_file
+        self._s.connect(self.socket_file)
+        self.backend_mod = backend_mod
+        self._connected = True
+        self._life = time.time() - random.randint(0, 10)
+        self._pool = pool
+
+    def __del__(self):
+        self.release()
+
+    def matches(self, **match_options):
+        target_sock = match_options.get('socket_file')
+        return target_sock == self.socket_file
+
+    def is_connected(self):
+        if self._connected:
+            return util.is_connected(self._s)
+        return False
+
+    def handle_exception(self, exception):
+        print('got an exception')
+        print(str(exception))
+
+    def get_lifetime(self):
+        return self._life
+
+    def invalidate(self):
+        self._s.close()
+        self._connected = False
+        self._life = -1
+
+    def release(self):
+        if self._pool is not None:
+            if self._connected:
+                self._pool.release_connection(self)
+            else:
+                self._pool = None
+
+    def send(self, data):
+        return self._s.send(data)
+
+    def recv(self, size=1024):
+        return self._s.recv(size)
+
 
 
 class TcpConnector(Connector):
